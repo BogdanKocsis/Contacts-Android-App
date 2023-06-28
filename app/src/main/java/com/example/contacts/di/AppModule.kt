9da8bf60da8bdca283fc8ce03fb7b002a1425b4b
bogdan.kocsis <bogdan.kocsis@fortech.ro>
2023-06-28 12:58:06 +0300

    Added contact model and app module for providing navigation manager.

---
+++ app/src/main/java/com/example/contacts/di/AppModule.kt
@@ -0,0 +1,17 @@
+package com.example.contacts.di
+
+import com.example.contacts.navigation.NavigationManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun providesNavigationManager(): NavigationManager = NavigationManager()
+}
